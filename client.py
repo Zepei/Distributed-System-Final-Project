7ef235284526f8ef33481212e9ee6eac49d4b344
Zepei Xu <xuzepei19950617@gmail.com>
2019-12-01 21:13:58 -0500

    give client default host

--- client.py
+++ client.py
@@ -54,6 +54,9 @@ top.protocol("WM_DELETE_WINDOW", on_closing)
 
 #----Now comes the sockets part----
 HOST = input('Enter host: ')
+if not HOST:
+    HOST = "ec2-3-91-101-44.compute-1.amazonaws.com"
+
 PORT = input('Enter port: ')
 if not PORT:
     PORT = 33000
