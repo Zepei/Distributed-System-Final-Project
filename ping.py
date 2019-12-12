0617100fec7bad32c71b07a59448174fe7d04125
Zepei Xu <xuzepei19950617@gmail.com>
2019-12-12 20:07:25 +0800

    mod

--- ping.py
+++ ping.py
@@ -1,6 +1,9 @@
 import socket
 import ssl
 from threading import Thread
+from datetime import datetime
+
+clients = []
 
 def ping_server(CALocation):
     hostname = 'ec2-3-91-101-44.compute-1.amazonaws.com'
@@ -21,16 +24,22 @@ def run_server(certfile, keyfile):
     bindsocket.bind(('ec2-3-91-101-44.compute-1.amazonaws.com', 33000))
     bindsocket.listen(5)
 
-    ACCEPT_THREAD = Thread(target=accept_incoming_connections, args=(bindsocket, context,))
-    ACCEPT_THREAD.start()
+    Thread(target=accept_incoming_connections, args=(bindsocket, context,)).start()
 
 
 def accept_incoming_connections(bindsocket, context):
     while True:
+        if len(clients) == 4:
+            continue
+
         newsocket, fromaddr = bindsocket.accept()
         connstream = context.wrap_socket(newsocket, server_side=True)
         try:
             Thread(target=deal_with_client, args=(connstream,)).start()
+            clients.append({"connstream": connstream, "fromaddr": fromaddr})
+            if len(clients) == 4:
+                broadcast_peers_info(clients)
+                ## clear_clients(clients)
         finally:
             connstream.shutdown(socket.SHUT_RDWR)
             connstream.close()
@@ -45,8 +54,33 @@ def deal_with_client(connstream):
 
 
 def do_something(connstream, data):
-    print('did something')
+    data = data.decode('UTF-8')
+    # handle socket close
+    if data == '':
+        connstream.close()
+        for i in range(len(clients)):
+            if clients[i]["connstream"] == connstream:
+                del clients[i]
+                break
+    # handle check_alive response
 
 
-def check_alive():
-    print('check alive')
+def broadcast_peers_info(clients):
+    for i in range(4):
+        if i == 0:
+            clients[i]["connstream"].send((clients[1]["fromaddr"]+" "+clients[2]["fromaddr"]+" "+clients[3]["fromaddr"]).encode('UTF-8'))
+        if i == 1:
+            clients[i]["connstream"].send((clients[2]["fromaddr"]+" "+clients[3]["fromaddr"]).encode('UTF-8'))
+        if i == 2:
+            clients[i]["connstream"].send((clients[3]["fromaddr"]).encode('UTF-8'))
+
+
+def clear_clients(clients):
+    del clients[:]
+
+
+def check_alive(clients):
+    # check if clients are alive
+    # send peers a specific message and expect get right reply in a timely manner
+    for client in clients:
+        client["connstream"].send(("alive?"+"time="+datetime.utcnow()).encode('UTF-8'))
