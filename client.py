227c210f478a135d9527f09efe9c9ab6e9ba10c4
Zepei Xu <xuzepei19950617@gmail.com>
2019-12-16 02:12:15 +0800

    two ports for match making and check alive, want to replace thread with selector

--- client.py
+++ client.py
@@ -3,40 +3,11 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 import tkinter
+import ping
 
-# hands = []
-
-
-# class Card:
-#     def __init__(self, suit, number):
-#         self.suit = suit
-#         self.number = number
-#
-#
-# def receive():
-#     """Handles receiving of messages."""
-#     while True:
-#         try:
-#             msg = client_socket.recv(BUFSIZ).decode("utf8")
-#             msg_list.insert(tkinter.END, msg)
-#         except OSError:  # Possibly client has left the chat.
-#             break
-
-
-# def send(event=None):  # event is passed by binders.
-#     """Handles sending of messages."""
-#     msg = my_msg.get()
-#     my_msg.set("")  # Clears input field.
-#     client_socket.send(bytes(msg, "utf8"))
-#     if msg == "{quit}":
-#         client_socket.close()
-#         top.quit()
-
-
-# def on_closing(event=None):
-#     """This function is to be called when the window is closed."""
-#     my_msg.set("{quit}")
-#     send()
+BUFSIZ = 1024
+match_socket = ''
+alive_socket = ''
 
 
 def cheat():
@@ -47,29 +18,37 @@ def play():
     print("you played")
 
 
-table_example_text= 'example!!! Heart 10; Space 13; Square 5'
-hand_example_text= 'example: Space Ace; Square five;'
+def start_match():
+    match_socket, alive_socket = ping.ping_server('')
+
+
+table_example_text= 'Heart 10; Space 13; Square 5'
+hand_example_text= 'Space Ace; Square five;'
 
 top = tkinter.Tk()
 top.title("The Game You Can't Cheat")
-main = tkinter.Frame(top, width=20, height=20)
+main = tkinter.Frame(top)
 main.pack()
 
 left_frame = tkinter.Frame(main)
-left_frame.grid(row=0, column=0)
-match_button = tkinter.Button(left_frame, text='Match!', highlightbackground='#3E4149')
+left_frame.pack(side='left')
+match_button = tkinter.Button(left_frame, text='Match!', highlightbackground='#3E4149', command=start_match)
 match_button.pack()
 
 right_frame = tkinter.Frame(main)
-right_frame.grid(row=0, column=1)
+right_frame.pack(side='right')
+info_frame = tkinter.Frame(right_frame)
+info_frame.pack()
+tkinter.Label(info_frame, text='You are player 1234').grid(row=0, column=0)
+tkinter.Label(info_frame, text="Any prompts show here: ").grid(row=1, column=0)
 table_frame = tkinter.Frame(right_frame)
 table_frame.pack()
-tkinter.Label(table_frame, text='on the table: ').grid(column=0)
-tkinter.Label(table_frame, text=table_example_text).grid(column=1)
+tkinter.Label(table_frame, text='On the table: ').grid(row=0,column=0)
+tkinter.Label(table_frame, text=table_example_text).grid(row=0, column=1)
 hand_frame = tkinter.Frame(right_frame)
 hand_frame.pack()
-tkinter.Label(hand_frame, text='your hand: ').grid(column=0)
-tkinter.Label(hand_frame, text=hand_example_text).grid(column=1)
+tkinter.Label(hand_frame, text='In your hand: ').grid(row=1, column=0)
+tkinter.Label(hand_frame, text=hand_example_text).grid(row=1, column=1)
 operation_frame = tkinter.Frame(right_frame)
 operation_frame.pack()
 suit = tkinter.StringVar()
@@ -85,47 +64,6 @@ play_button.grid(row=0, column=1)
 cheat_button = tkinter.Button(operation_frame, text="Cheat!", command=cheat, highlightbackground='#3E4149')
 cheat_button.grid(row=1, column=1)
 
-# messages_frame = tkinter.Frame(top)
-# my_msg = tkinter.StringVar()  # For the messages to be sent.
-# my_msg.set("Type your messages here.")
-# scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
-# # Following will contain the messages.
-# msg_list = tkinter.Listbox(messages_frame, height=10, width=50, yscrollcommand=scrollbar.set)
-# scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-# msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-# msg_list.pack()
-# messages_frame.pack()
 
-
-
-# entry_field = tkinter.Entry(top, textvariable=my_msg)
-# entry_field.bind("<Return>", send)
-# entry_field.pack()
-# send_button = tkinter.Button(top, text="Send", command=send, highlightbackground='#3E4149')
-# send_button.pack()
-
-
-
-# top.protocol("WM_DELETE_WINDOW", on_closing)
-
-#----Now comes the sockets part----
-# HOST = input('Enter host(leave blank if not known): ')
-# if not HOST:
-#     HOST = "ec2-3-91-101-44.compute-1.amazonaws.com"
-# else: HOST = ''
-#
-# PORT = input('Enter port(leave blank if not known): ')
-# if not PORT:
-#     PORT = 33000
-# else:
-#     PORT = int(PORT)
-#
-# BUFSIZ = 1024
-# ADDR = (HOST, PORT)
-#
-# client_socket = socket(AF_INET, SOCK_STREAM)
-# client_socket.connect(ADDR)
-#
-# receive_thread = Thread(target=receive)
-# receive_thread.start()
-tkinter.mainloop()  # Starts GUI execution.
+# Starts GUI execution.
+tkinter.mainloop()
--- ping.py
+++ ping.py
@@ -1,48 +1,106 @@
 import socket
+import selectors
 import ssl
-from threading import Thread
+from threading import Thread, Timer
 from datetime import datetime
 
+sel = selectors.DefaultSelector()
 clients = []
+check_alive_clients = []
+check_alive_last_time_stamp = 0
 
 def ping_server(CALocation):
     hostname = 'ec2-3-91-101-44.compute-1.amazonaws.com'
-    port = 33000
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    context.load_verify_locations(CALocation)
-    conn = context.wrap_socket(sock)
-    conn.connect(hostname, port)
-    return conn
+
+    # match making connection
+    match_port = 33000
+    match_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    match_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    match_context.load_verify_locations(CALocation)
+    match_conn = match_context.wrap_socket(match_sock)
+
+    # check_alive connection
+    alive_port = 34000
+    alive_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    alive_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    alive_context.load_verify_locations(CALocation)
+    alive_conn = alive_context.wrap_socket(alive_sock)
+
+    match_conn.connect(hostname, match_port)
+    match_conn.connect(hostname, alive_port)
+
+    return match_conn, alive_conn
 
 
 def run_server(certfile, keyfile):
-    context = ssl.create_default_context((ssl.Purpose.CLIENT_AUTH))
-    context.load_cert_chain(certfile=certfile, keyfile=keyfile)
+    match_context = ssl.create_default_context((ssl.Purpose.CLIENT_AUTH))
+    match_context.load_cert_chain(certfile=certfile, keyfile=keyfile)
 
-    bindsocket = socket.socket()
-    bindsocket.bind(('ec2-3-91-101-44.compute-1.amazonaws.com', 33000))
-    bindsocket.listen(5)
+    alive_context = ssl.create_default_context((ssl.Purpose.CLIENT_AUTH))
+    alive_context.load_cert_chain(certfile=certfile, keyfile=keyfile)
 
-    Thread(target=accept_incoming_connections, args=(bindsocket, context,)).start()
+    match_socket = socket.socket()
+    match_socket.bind('ec2-3-91-101-44.compute-1.amazonaws.com', 33000)
+    match_socket.listen(5)
+    # match_socket.setblocking(False)
+    # sel.register(match_socket, selectors.EVENT_READ, accept_incoming_connections)
+
+    alive_socket = socket.socket()
+    alive_socket.bind('ec2-3-91-101-44.compute-1.amazonaws.com', 34000)
+    alive_socket.listen(5)
+    # alive_socket.setblocking(False)
+    # sel.register(alive_socket, selectors.EVENT_READ, alive_accept_incoming_connections)
+
+    Thread(target=accept_incoming_connections, args=(match_socket, match_context,)).start()
+    Thread(target=alive_accept_incoming_connections, args=(alive_socket, alive_context,)).start()
+
+    check_alive_interval = Timer(30, check_alive).start()
 
 
-def accept_incoming_connections(bindsocket, context):
+def alive_accept_incoming_connections(alive_socket, alive_context):
+    while True:
+        if len(check_alive_clients) == 4:
+            continue
+        newsocket, fromaddr = alive_socket.accept()
+        connstream = alive_context.wrap_socket(newsocket, server_side=True)
+        try:
+            check_alive_clients.append({"connstream": connstream, "fromaddr": fromaddr, "last_alive_time": int(datetime.utcnow())})
+            Thread(target=alive_deal_with_client, args=(connstream,)).start()
+        finally:
+            for i in range(len(check_alive_clients)):
+                if check_alive_clients[i]["connstream"] == connstream:
+                    del check_alive_clients[i]
+                    break
+            connstream.shutdown(socket.SHUT_RDWR)
+            connstream.close()
+
+
+def accept_incoming_connections(match_socket, match_context):
     while True:
         if len(clients) == 4:
             continue
-
-        newsocket, fromaddr = bindsocket.accept()
-        connstream = context.wrap_socket(newsocket, server_side=True)
+        newsocket, fromaddr = match_socket.accept()
+        connstream = match_context.wrap_socket(newsocket, server_side=True)
         try:
-            Thread(target=deal_with_client, args=(connstream,)).start()
             clients.append({"connstream": connstream, "fromaddr": fromaddr})
+            Thread(target=deal_with_client, args=(connstream,)).start()
             if len(clients) == 4:
                 broadcast_peers_info(clients)
-                ## clear_clients(clients)
         finally:
+            for i in range(len(clients)):
+                if clients[i]["connstream"] == connstream:
+                    del clients[i]
+                    break
             connstream.shutdown(socket.SHUT_RDWR)
             connstream.close()
+
+
+def alive_deal_with_client(connstream):
+    data = connstream.recv(1024)
+    while data:
+        if not alive_do_something(connstream, data):
+            break
+        data = connstream.recv(1024)
 
 
 def deal_with_client(connstream):
@@ -53,16 +111,19 @@ def deal_with_client(connstream):
         data = connstream.recv(1024)
 
 
+# assume the response is like 'alive!time=1234567890'
+def alive_do_something(connstream, data):
+    data = data.decode('UTF-8')
+    if data.find('alive!') > 0:
+        time = int(data[data.find('=')+1:])
+        for i in range(len(check_alive_clients)):
+            if check_alive_clients[i]["connstream"] == connstream:
+                check_alive_clients[i]["last_alive_time"] = time
+                break
+
+
 def do_something(connstream, data):
     data = data.decode('UTF-8')
-    # handle socket close
-    if data == '':
-        connstream.close()
-        for i in range(len(clients)):
-            if clients[i]["connstream"] == connstream:
-                del clients[i]
-                break
-    # handle check_alive response
 
 
 def broadcast_peers_info(clients):
@@ -75,12 +136,27 @@ def broadcast_peers_info(clients):
             clients[i]["connstream"].send((clients[3]["fromaddr"]).encode('UTF-8'))
 
 
-def clear_clients(clients):
+def clear_clients():
     del clients[:]
+    del check_alive_clients[:]
 
 
-def check_alive(clients):
-    # check if clients are alive
-    # send peers a specific message and expect get right reply in a timely manner
-    for client in clients:
-        client["connstream"].send(("alive?"+"time="+datetime.utcnow()).encode('UTF-8'))
+def check_alive():
+    check_alive_last_time_stamp = datetime.utcnow()
+    for client in check_alive_clients:
+        if check_alive_last_time_stamp - client["last_alive_time"] > 1000000 * 30:
+            print("This client is offline: "+client)
+            for i in range(len(check_alive_clients)):
+                if check_alive_clients[i]["connstream"] == client:
+                    check_alive_clients[i]["connstream"].shutdown(socket.SHUT_RDWR)
+                    check_alive_clients[i]["connstream"].close()
+                    del check_alive_clients[i]
+                    break
+            for i in range(len(clients)):
+                if clients[i]["connstream"] == client:
+                    clients[i]["connstream"].shutdow(socket.SHUT_RDWR)
+                    clients[i]["connstream"].close()
+                    del clients[i]
+                    break
+    for client in check_alive_clients:
+        client["connstream"].send(("alive?" + "time=" + check_alive_last_time_stamp).encode('UTF-8'))
